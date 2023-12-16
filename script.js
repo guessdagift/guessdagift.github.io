8a6c37b1b72d4f9ed4e2b08b64a64b19357bdd13
guessdagift <154005265+guessdagift@users.noreply.github.com>
2023-12-16 10:30:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function checkGuess(){
+  var userGuess = document.getElementByID("userGuess").value;
+  var correctAnswer = "Sowing the seeds of love";
 
+  if (userGuess === correctAnswer){
+    window.location.href = 'winner.html';
+  } else{
+    alert("Incorrect guess. Try again!");
+  }
+}
